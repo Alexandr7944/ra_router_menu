9d62dce398512ec132813c0efe698ce90615887f
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2023-07-02 13:13:20 +0300

    change path

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,8 @@
 import { DriftPage, ForzaPage, HomePage, TimeAttackPage } from "../components";
 
 export const routes = [
-  { path: '/', element: HomePage },
-  { path: '/drift', element: DriftPage },
-  { path: '/timeattack', element: TimeAttackPage },
-  { path: '/forza', element: ForzaPage },
+  { path: '/*', element: HomePage },
+  { path: '/ra_router_menu/drift', element: DriftPage },
+  { path: '/ra_router_menu/timeattack', element: TimeAttackPage },
+  { path: '/ra_router_menu/forza', element: ForzaPage },
 ]
